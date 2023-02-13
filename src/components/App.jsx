088db5733f55f6e8568ad76595333d13f7844ad7
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2023-02-13 20:25:31 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,28 +5,27 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Searchbar } from './Searchbar/Searchbar';
 
-const THEKEY = '31673863-7b4e2329a784886b2ded53b03&';
-const safeSearch = false;
-let totalHits = 0;
-let page = 1;
-let amount = 12;
-let totalPages = 1;
-const getUrl = search =>
-  `https://pixabay.com/api/?key=${THEKEY}&q=${search}&type=photo&orientation=horizontal&safesearch=${safeSearch}&per_page=${amount}&page=${page}`;
-
 export class App extends React.Component {
   state = {
     pictures: [],
     fetchingImages: false,
+    THEKEY: '31673863-7b4e2329a784886b2ded53b03&',
     actualSearch: '',
+    safeSearch: false,
+    totalHits: -1,
+    page: 1,
+    amount: 12,
+    totalPages: -1,
   };
+  getUrl = search =>
+    `https://pixabay.com/api/?key=${this.state.THEKEY}&q=${search}&type=photo&orientation=horizontal&safesearch=${this.state.safeSearch}&per_page=${this.state.amount}&page=${this.state.page}`;
   handleFetchPictures = async value => {
     const parsedName = value.trim();
     if (parsedName.length === 0) return;
-    const url = getUrl(parsedName);
+    // const url = this.getUrl(parsedName);
     this.setState({ fetchingImages: true });
     try {
-      const response = await fetch(url);
+      const response = await fetch(this.getUrl(parsedName));
       if (!response.ok) {
         throw (
           (new Error(`Fetch failed with status ${response.status}`),
@@ -42,16 +41,20 @@ export class App extends React.Component {
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
-      totalHits = data.totalHits;
-      if (totalHits === 0) return;
-      const pictures = data.hits.map(picture => ({
-        id: picture.id,
-        webformatURL: picture.webformatURL,
-        largeImageURL: picture.largeImageURL,
-        tags: picture.tags,
-      }));
-      const newPictures = [...this.state.pictures, ...pictures];
-      this.setState({ pictures: newPictures });
+      this.setState({ totalHits: data.totalHits }, () => {
+        console.log('handlefetchpictures', this.state.totalHits);
+        if (this.state.totalHits === 0) return;
+        else {
+          const pictures = data.hits.map(picture => ({
+            id: picture.id,
+            webformatURL: picture.webformatURL,
+            largeImageURL: picture.largeImageURL,
+            tags: picture.tags,
+          }));
+          const newPictures = [...this.state.pictures, ...pictures];
+          this.setState({ pictures: newPictures });
+        }
+      });
     } catch (error) {
       console.error(error);
     }
@@ -60,22 +63,29 @@ export class App extends React.Component {
     window.scrollBy({ top: cardHeight * 3, behavior: 'smooth' });
   };
 
-  handlePaginationLoader = () => {
-    const { actualSearch } = this.state;
-    if (page < totalPages) {
-      return (page += 1) && this.handleFetchPictures(actualSearch);
-    }
-  };
-
   handleSubmit = async (event, value) => {
     event.preventDefault();
-    this.setState({ actualSearch: value, pictures: [] });
-    page = 1;
-    totalHits = 0;
+    this.setState({ actualSearch: value, pictures: [], totalHits: 0, page: 1 });
+    console.log('before', this.state.totalHits);
     await this.handleFetchPictures(value);
+    const { totalHits, amount } = this.state;
+    console.log('after', totalHits);
     if (totalHits === 0) return;
-    totalPages = Math.ceil(totalHits / amount);
+    const totalPagesAmount = Math.ceil(totalHits / amount);
+    this.setState({ totalPages: totalPagesAmount });
     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  };
+
+  handlePaginationLoader = () => {
+    const { actualSearch, page, totalPages } = this.state;
+    if (page < totalPages) {
+      this.setState(
+        prevState => ({ page: prevState.page + 1 }),
+        () => {
+          this.handleFetchPictures(actualSearch);
+        }
+      );
+    }
   };
 
   render() {
@@ -86,8 +96,8 @@ export class App extends React.Component {
         {this.state.pictures.length > 0 && (
           <ImageGallery
             pictures={this.state.pictures}
-            page={page}
-            totalPages={totalPages}
+            page={this.state.page}
+            totalPages={this.state.totalPages}
             onButtonClick={this.handlePaginationLoader}
             onScroll={this.handleScrollPage}
           />
